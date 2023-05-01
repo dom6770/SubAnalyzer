c3ffc02a6e015865e1d09811457e50604c85bdfd
dom6770 <github.me@neus.xyz>
2023-05-01 15:09:41 +0200

    Build class template

--- SubAnalyzer/Program.cs
+++ SubAnalyzer/Program.cs
@@ -1,3 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
+﻿using System;
+class Program {
+    private static void Main(string[] args) {
+        string path;
+
+        if(args.Length > 0 && Directory.Exists(args[0])){
+            path = args[0];
+        } else {
+            while(true) {
+                Console.Write("Please enter a path: ");
+                string input = Console.ReadLine();
+
+                if(Directory.Exists(input)) {
+                    path = input;
+                    break;
+                } else {
+                    int currentLineCursor = Console.CursorTop - 1;
+                    Console.Write("Invalid, please try again!");
+                    Console.SetCursorPosition(0,currentLineCursor);
+                    Console.Write(new string(' ',Console.BufferWidth - 1));
+                    Console.SetCursorPosition(0,currentLineCursor);
+                }
+            }
+        }
+        Console.SetCursorPosition(0,Console.CursorTop);
+        Console.Write(new string(' ',Console.BufferWidth - 1));
+        Console.SetCursorPosition(0,Console.CursorTop);
+
+        Console.WriteLine($"Path: {path}");
+
+        SubAnalyzer subAnalyzer = new(path);
+        subAnalyzer.Run();
+    }
+}
---
+++ SubAnalyzer/SubAnalyzer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class SubAnalyzer {
+    public string Path { get; set; }
+    public SubAnalyzer(string arg) {
+        Path = arg;        
+    }
+    public void Run() {
+        Console.WriteLine($"Time: {DateTime.Now.ToString("ddd dd.MM.yyyy HH:mm:ss")}\n");
+        Console.Write(Path);
+    }
+}
